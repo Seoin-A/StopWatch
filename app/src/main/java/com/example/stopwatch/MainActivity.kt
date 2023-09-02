80cd5d896145b9a5626efd64fffc6291e1e19323
seoin2356 <seoin2389@icloud.com>
2023-09-02 23:57:15 +0900

    - string 외부 파일 수정 - srart  기능 구현 - error 발생 확인

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() , View.OnClickListener {
 
@@ -18,6 +20,9 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
 
     private var isRunning = false
 
+    private var timer : Timer? = null
+    private var time = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,27 +33,45 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
         tv_minute = findViewById(R.id.tv_minute)
         tv_second = findViewById(R.id.tv_second)
         tv_millisecond = findViewById(R.id.tv_millisecond)
+
+        btn_start.setOnClickListener(this)
+        btn_refresh.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         when(view?.id){
             R.id.btn_start -> {
                 if(isRunning){
-                    pause()
+                    pause() // 일시정지
                 }else{
-                    start()
+                    start() // 시작
                 }
             }
             R.id.btn_refresh -> {
-                refresh()
+                refresh() // 초기화
             }
 
         }
     }
 
-
     private fun start(){
+        btn_start.text = getString(R.string.start)
+        btn_start.setBackgroundColor(getColor(R.color.btn_pause))
+        isRunning = true
 
+        timer = timer(period = 10){
+            // 1000ms = 1s
+            // 0.01 time 1++
+            time++
+
+            val milli_second = time % 100
+            val second = (time % 6000) / 100
+            val minute = time / 6000
+
+            tv_millisecond.text = if(milli_second <10 ) ".0${milli_second}" else ".${milli_second}"
+            tv_second.text =if(second < 10) "0${second}" else ".${second}"
+            tv_minute.text = "${minute}"
+        }
     }
 
     private fun pause(){
